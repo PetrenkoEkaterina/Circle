4cee6f435f2b4f439035bc782b0f69b3df8e6a39
PetrenkoEkaterina <shefbest@gmail.com>
2023-10-30 23:33:47 +0300

    New

---
+++ src/main/java/Circle.java
@@ -0,0 +1,8 @@
+public class Circle {
+    public static void main(String[] args) {
+        double radius = 4;
+        double area = Math.PI * (radius * radius);
+
+        System.out.println("Circle area = " + area);
+    }
+}
